ef33e33e649d5ce071f19499f1a1ccf0f05f7ef8
gagan884 <brargagan964@gmail.com>
2022-11-10 16:39:50 +0530

    First Commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,19 +10,19 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   postProduct(data: any){
-    return this.http.post<any>("http://localhost:3000/productlist/",data)
+    return this.http.post<any>("http://localhost:44331/productlist/",data)
   }
   getProduct(){
-       return this.http.get<any>("http://localhost:3000/productlist/")
+       return this.http.get<any>("http://localhost:44331/productlist/")
 
   }
   putProduct(data:any,id:number){
-    return this.http.put<any>("http://localhost:3000/productlist/"+id,data);
+    return this.http.put<any>("http://localhost:44331/productlist/"+id,data);
 
   }
   deleteProduct(id:number)
   {
-    return this.http.delete<any>("http://localhost:3000/productlist/"+id);
+    return this.http.delete<any>("http://localhost:44331/productlist/"+id);
 
 
   }
